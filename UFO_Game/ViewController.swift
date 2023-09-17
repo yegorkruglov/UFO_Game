55f4ac62dc7b39a1559e423d7f32e6228a2fdd70
Egor Kruglov <yegorkruglov@gmail.com>
2023-09-17 13:46:16 +0300

    Initial Commit

---
+++ UFO_Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UFO_Game
+//
+//  Created by Egor Kruglov on 17.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
