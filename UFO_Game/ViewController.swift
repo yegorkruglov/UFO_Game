1710494c8cdca22f0d19ca642566f279e40ecde6
Egor Kruglov <yegorkruglov@gmail.com>
2023-09-17 15:29:03 +0300

    project setup, plane, buttons and enemies spawn added

--- UFO_Game/ViewController.swift
+++ UFO_Game/ViewController.swift
@@ -1,19 +1,119 @@
 //
 //  ViewController.swift
-//  UFO_Game
+//  PlaneGame
 //
-//  Created by Egor Kruglov on 17.09.2023.
+//  Created by Egor Kruglov on 08.09.2023.
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    lazy var planeView = {
+        let plane = UIView()
+        plane.backgroundColor = .blue
+        
+        return plane
+    }()
+    lazy var stackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 10
+        stack.alignment = .fill
+        stack.distribution = .fillEqually
+        stack.backgroundColor = .black
+        
+        return stack
+    }()
+    lazy var leftButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .green
+        button.setImage(UIImage(systemName: "arrowshape.left.fill"), for: .normal)
+        button.addTarget(self, action: #selector(moveLeft), for: .touchUpInside)
+        
+        return button
+    }()
+    lazy var rightButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .green
+        button.setImage(UIImage(systemName: "arrowshape.right.fill"), for: .normal)
+        button.addTarget(self, action: #selector(moveRight), for: .touchUpInside)
 
+        return button
+    }()
+    
+    private var isCollided = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .lightGray
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(leftButton)
+        stackView.addArrangedSubview(rightButton)
+        view.addSubview(planeView)
+        
+        
+        stackView.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().inset(20)
+            make.left.equalToSuperview().inset(20)
+            make.right.equalToSuperview().inset(20)
+            make.height.equalTo(80)
+        }
+        
+        planeView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(stackView.snp.top).inset(-20)
+            make.height.equalTo(100)
+            make.width.equalTo(100)
+        }
+        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        Timer.scheduledTimer(
+            timeInterval: 2.0,
+            target: self,
+            selector: #selector(generateEnemy),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    @objc private func generateEnemy() {
+        let minX = view.frame.width / 8
+        let maxX = view.frame.width - 20
+        
+        let enemyView = UIView(
+            frame: CGRect(x: CGFloat.random(in: minX...maxX),
+                          y: -100,
+                          width: 100,
+                          height: 100)
+        )
+        enemyView.backgroundColor = .red
+        DispatchQueue.main.async {
+            self.view.addSubview(enemyView)
+        }
+        
+        UIView.animate(withDuration: 5, animations: { enemyView.frame.origin.y += 1000 }) { _ in
+            enemyView.removeFromSuperview()
 
-
+        }
+    }
+    
+    @objc private func moveLeft() {
+        UIView.animate(withDuration: 0.1, animations: {
+            self.planeView.frame.origin.x -= 50
+        })
+    }
+    
+    @objc private func moveRight() {
+        UIView.animate(withDuration: 0.1, animations: {
+            self.planeView.frame.origin.x += 50
+        })
+    }
 }
 
