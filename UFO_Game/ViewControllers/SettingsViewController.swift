c6027b78c2d41e26c534687b8dd74d2af2f6d789
Egor Kruglov <yegorkruglov@gmail.com>
2023-09-20 20:11:05 +0300

    settingsVC ui improvements

--- UFO_Game/ViewControllers/SettingsViewController.swift
+++ UFO_Game/ViewControllers/SettingsViewController.swift
@@ -25,12 +25,34 @@ class SettingsViewController: UIViewController {
         return label
     }()
     
+    private lazy var iconsLabel = {
+        let label = UILabel()
+        label.text = "Choose fighter, enemies, missiles"
+        label.textAlignment = .center
+        label.textColor = .white
+        label.backgroundColor = .clear
+        label.adjustsFontSizeToFitWidth = true
+        
+        return label
+    }()
+    
+    private lazy var stackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 10
+        stack.alignment = .fill
+        stack.distribution = .fillEqually
+        
+        return stack
+    }()
+    private lazy var saveButton = getGameButton(selector: #selector(saveSettings), title: "SAVE")
+    private lazy var dismissButton = getGameButton(selector: #selector(dismissSettings), title: "CANCEL")
+    
     private lazy var nameTextField = {
         let tf = UITextField()
         tf.placeholder = "Player"
         tf.backgroundColor = .white
         tf.textAlignment = .center
-        tf.layer.cornerRadius = tf.bounds.height / 2
         
         return tf
     }()
@@ -41,16 +63,37 @@ class SettingsViewController: UIViewController {
         
         setupUI()
     }
+    
+    deinit{
+        print("SettingsVC was realesed")
+    }
+    @objc private func saveSettings() {
+        
+    }
+    
+    @objc private func dismissSettings() {
+        dismiss(animated: true)
+    }
 }
 
 extension SettingsViewController {
     private func setupUI() {
         view.addSubview(collectionView)
         collectionView.backgroundColor = .white
+        collectionView.layer.cornerRadius = 20
         collectionView.snp.makeConstraints { make in
             make.center.equalToSuperview()
             make.width.equalToSuperview().inset(16)
             make.height.equalToSuperview().dividedBy(2)
+        }
+        
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(saveButton)
+        stackView.addArrangedSubview(dismissButton)
+        stackView.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(collectionView)
+            make.top.equalTo(collectionView.snp.bottom).inset(-20)
+            make.height.equalTo(view.frame.height / 10)
         }
         
         view.addSubview(nameTextField)
@@ -60,7 +103,7 @@ extension SettingsViewController {
             make.bottom.equalTo(collectionView.snp.top).inset(-20)
             make.height.equalToSuperview().dividedBy(16)
         }
-        nameTextField.layer.cornerRadius = 10
+        nameTextField.layer.cornerRadius = 20
         
         view.addSubview(nameLabel)
         nameLabel.snp.makeConstraints { make in
